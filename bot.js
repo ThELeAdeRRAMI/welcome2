da246a4b661a162ba182b7e8d6cc92a73a306c91
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-20 09:54:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,12 @@ if(mohamed) {
 setTimeout(() => mohamed.send(`*Welcome To* ***Fèarless*** *Server*..`), 4000)        
 }
 });
+
+client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("514147258795687938");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`__ WēŁçØmĘ Ťö ** ŤhË_ŁèĄđëŘş ** ŞėŘvËŕ... __`), 4000)        
+}
+});
 client.login(process.env.BOT_TOKEN); 
