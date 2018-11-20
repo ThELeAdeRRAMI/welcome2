cab5d8ddb3243861a3344741e0b179dfdb307439
ThELeAdeRRAMI <44674539+ThELeAdeRRAMI@users.noreply.github.com>
2018-11-20 09:52:12 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿ const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("512734412501155840");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`*Welcome To* ***Fèarless*** *Server*..`), 4000)        
+}
+});
+client.login(process.env.BOT_TOKEN); 
